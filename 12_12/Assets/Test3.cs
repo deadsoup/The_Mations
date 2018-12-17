db2f90cf63e974e1411e4e55c76603786a2dc16d
DESKTOP-OB8QAVA\6kigs1_22 <zere4579@naver.com>
2018-12-17 16:48:35 +0900

    hair

---
+++ 12_12/Assets/Test3.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+using System.IO;
+
+public class Test3 : MonoBehaviour
+{
+    //플레이어 클래스
+    public class PlayerInfo 
+    {
+        public int Id;
+        public string Name;
+        public double Gold;
+        public PlayerInfo(int id, string name, double gold)
+        {
+            Id = id;
+            Name = name;
+            Gold = gold;
+
+        }
+
+
+
+
+    }
+
+    public List<PlayerInfo> playerInfoList = new List<PlayerInfo>();
+
+    //public playerInfo[] playerList = new playerInfo[5];
+
+
+    void SaveJson()
+    {
+        print("Save Json File");
+        playerInfoList.Add(new PlayerInfo(1, "아리수", 100));
+        playerInfoList.Add(new PlayerInfo(1, "김수한무", 1000));
+        playerInfoList.Add(new PlayerInfo(1, "김한수", 10000));
+        playerInfoList.Add(new PlayerInfo(1, "무리수", 100000));
+
+        JsonData jsonData = JsonMapper.ToJson(playerInfoList);
+
+        File.WriteAllText(Application.dataPath + "/JsonData/PlayerInfoData.json", jsonData.ToString()); 
+        //파일에 전부 텍스트로 쓴다 (어플리케이션가 설치된 주소를 기록 + "Json파일을 설치할 폴더 위치와 파일명 기입", 그리고 jsonData를 문자열로 해달라)
+
+    }
+    string jsonString;
+
+    void LoadJson()
+    {
+
+        jsonString = "";
+        jsonString = File.ReadAllText(Application.dataPath + "/JsonData/PlayerInfoData.json");
+        JsonData playerData = JsonMapper.ToObject(jsonString);
+        
+
+
+
+        for (int i = 0; i < playerData.Count; i++)
+        {
+            print(playerData[i]["Id"] + "," + playerData[i]["Name"] + "," + playerData[i]["Gold"]);
+        }
+
+
+
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //SaveJson();
+        LoadJson();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 12_12/Assets/test.cs
@@ -0,0 +1,365 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    public GameObject[] hairButton = new GameObject[9];
+
+    public GameObject[] upbodyButton = new GameObject[6];
+
+    public GameObject[] downbodyButton = new GameObject[6];
+
+    public GameObject[] hair = new GameObject[9];
+
+    public GameObject[] upbody = new GameObject[6];
+                          
+    public GameObject[] downbody = new GameObject[6];
+    
+    
+
+
+
+    int Hair;
+    static int HairCount;
+
+    int UpBody;
+    static int UpBodyCount;
+
+    int DownBody;
+    static int DownBodyCount;
+
+    static int saveHair;
+    static int saveUp;
+    static int saveDown;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        
+        hair[0].SetActive(true);
+        upbody[5].SetActive(true);
+        downbody[5].SetActive(true);
+        
+    }
+
+    public void save()
+    {
+        PlayerPrefs.SetInt("Hair", HairCount);
+        PlayerPrefs.SetInt("Up", UpBodyCount);
+        PlayerPrefs.SetInt("Down", DownBodyCount);
+
+        Debug.Log(HairCount);                                                                                                                                                                                                                                                                                                  
+        Debug.Log(UpBodyCount);
+        Debug.Log(DownBodyCount);
+
+    }
+    
+    public void load()
+    {
+        HairCount = PlayerPrefs.GetInt("Hair");
+        UpBodyCount = PlayerPrefs.GetInt("Up");
+        DownBodyCount = PlayerPrefs.GetInt("Down");
+
+        int c;
+        for (c = 0; c <= 8; c++)
+        {
+            hair[c].SetActive(false);
+        }
+
+        int e;
+        for (e = 0; e <= 5; e++)
+        {
+            upbody[e].SetActive(false);
+        }
+
+        int f;
+        for (f = 0; f <= 5; f++)
+        {
+            downbody[f].SetActive(false);
+        }
+
+
+
+
+        hair[HairCount].SetActive(true);
+        upbody[UpBodyCount].SetActive(true);
+        downbody[DownBodyCount].SetActive(true);
+
+
+        Debug.Log(HairCount);
+        Debug.Log(UpBodyCount);
+        Debug.Log(DownBodyCount);
+
+    }
+
+    
+
+
+
+
+    public void changeHair1()
+    {
+
+        HairCount = 0;
+        int c;
+        for (c = 0; c <= 8; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+    public void changeHair2()
+    {
+        HairCount = 1;
+        int c;
+        for (c = 0; c < 9; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+    public void changeHair3()
+    {
+        HairCount = 2;
+        int c;
+        for (c = 0; c < 9; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+    public void changeHair4()
+    {
+        HairCount = 3;
+        int c;
+        for (c = 0; c < 9; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+    public void changeHair5()
+    {
+        HairCount = 4;
+        int c;
+        for (c = 0; c < 9; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+    public void changeHair6()
+    {
+        HairCount = 5;
+        int c;
+        for (c = 0; c < 9; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+    public void changeHair7()
+    {
+        HairCount = 6;
+        int c;
+        for (c = 0; c < 9; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+    public void changeHair8()
+    {
+        HairCount = 7;
+        int c;
+        for (c = 0; c < 9; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+    public void changeHair9()
+    {
+        HairCount = 8;
+        int c;
+        for (c = 0; c < 9; c++)
+        {
+            hair[c].SetActive(false);
+        }
+        hair[HairCount].SetActive(true);
+
+    }
+
+
+
+    public void changeUp1()
+    {
+        UpBodyCount = 0;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            upbody[c].SetActive(false);
+        }
+        upbody[UpBodyCount].SetActive(true);      
+
+    }
+
+    public void changeUp2()
+    {
+        UpBodyCount = 1;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            upbody[c].SetActive(false);
+        }
+        upbody[UpBodyCount].SetActive(true);
+
+    }
+
+    public void changeUp3()
+    {
+        UpBodyCount = 2;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            upbody[c].SetActive(false);
+        }
+        upbody[UpBodyCount].SetActive(true);
+
+    }
+
+    public void changeUp4()
+    {
+        UpBodyCount = 3;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            upbody[c].SetActive(false);
+        }
+        upbody[UpBodyCount].SetActive(true);
+
+    }
+
+    public void changeUp5()
+    {
+        UpBodyCount = 4;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            upbody[c].SetActive(false);
+        }
+        upbody[UpBodyCount].SetActive(true);
+
+    }
+
+    public void changeUp6()
+    {
+        UpBodyCount = 5;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            upbody[c].SetActive(false);
+        }
+        upbody[UpBodyCount].SetActive(true);
+
+    }
+
+    public void changeDown1()
+    {
+        DownBodyCount = 0;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            downbody[c].SetActive(false);
+        }
+        downbody[DownBodyCount].SetActive(true);
+
+    }
+
+    public void changeDonw2()
+    {
+        DownBodyCount = 1;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            downbody[c].SetActive(false);
+        }
+        downbody[DownBodyCount].SetActive(true);
+
+    }
+
+    public void changeDonw3()
+    {
+        DownBodyCount = 2;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            downbody[c].SetActive(false);
+        }
+        downbody[DownBodyCount].SetActive(true);
+
+    }
+
+    public void changeDonw4()
+    {
+        DownBodyCount = 3;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            downbody[c].SetActive(false);
+        }
+        downbody[DownBodyCount].SetActive(true);
+
+    }
+
+    public void changeDonw5()
+    {
+        DownBodyCount = 4;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            downbody[c].SetActive(false);
+        }
+        downbody[DownBodyCount].SetActive(true);
+
+    }
+
+    public void changeDonw6()
+    {
+        DownBodyCount = 5;
+        int c;
+        for (c = 0; c <= 5; c++)
+        {
+            downbody[c].SetActive(false);
+        }
+        downbody[DownBodyCount].SetActive(true);
+
+    }
+
+    
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+
+	}
+}
