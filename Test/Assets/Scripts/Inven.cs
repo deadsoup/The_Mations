840de50b39313d966f2eeb482b857b4bb7bfbdf6
deadsoup <44824297+deadsoup@users.noreply.github.com>
2018-12-20 22:09:53 +0900

    System 1

--- Test/Assets/Scripts/Inven.cs
+++ Test/Assets/Scripts/Inven.cs
@@ -6,15 +6,39 @@ public class Inven : MonoBehaviour
 {
 
 
+    public static int[] Id = new int[6];
+    public static string[] Name = new string[6];
+    public static string[] Text = new string[6];
+    public static int[] MaxHp = new int[6];
+    public static int[] MaxMp = new int[6];
+    public static int[] Str = new int[6];
+    public static int[] Dex = new int[6];
+    public static int[] Wis = new int[6];
 
+    int slotAmount;
 
+    public GameObject Invenpanel;
+    public GameObject Slotpanel;
+    public GameObject InvenSlot;
+    public GameObject InvenItem;
+
+    public List<Reward> items = new List<Reward>();
+    public List<GameObject> slots = new List<GameObject>();
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        slotAmount = 9;
+        for (int i = 0; i < slotAmount; i++)
+        {
+            items.Add(new Reward());
+            slots.Add(Instantiate(InvenSlot));
+            slots[i].transform.SetParent(Slotpanel.transform);
+        }
+
+
     }
 
     // Update is called once per frame
---
+++ Test/Assets/Scripts/Reward.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Reward : MonoBehaviour
+{
+
+    [System.Serializable]
+    public class RewardScript
+    {
+        public RewardScriptData[] data;
+    }
+
+    [System.Serializable]
+    public class RewardScriptData
+    {
+        public int Id;
+        public string Name;
+        public string Text;
+        public int MaxHp;
+        public int MaxMp;
+        public int Str;
+        public int Dex;
+        public int Wis;
+    }
+
+    RewardScript reward;
+
+
+
+
+    void Awake()
+    {
+        reward = JsonUtility.FromJson<RewardScript>(File.ReadAllText(Application.dataPath + "/ItemData.json"));
+    }
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < Inven.Id.Length; i++)
+        {
+            Inven.Id[i] = reward.data[i].Id;
+            Inven.Name[i] = reward.data[i].Name;
+            Inven.Text[i] = reward.data[i].Text;
+            Inven.MaxHp[i] = reward.data[i].MaxHp;
+            Inven.MaxMp[i] = reward.data[i].MaxMp;
+            Inven.Str[i] = reward.data[i].Str;
+            Inven.Dex[i] = reward.data[i].Dex;
+            Inven.Wis[i] = reward.data[i].Wis;
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
