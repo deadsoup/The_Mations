5c5cef1d82fc3e442158075287e11621a74419fc
FQUARTER <44913887+FQUARTER@users.noreply.github.com>
2019-01-03 17:21:39 +0900

    Yj_2019.01.03_1

--- The Mations proto Type_12/Assets/Scripts/Menu/CharInfo.cs
+++ The Mations proto Type_12/Assets/Scripts/Menu/CharInfo.cs
@@ -12,13 +12,25 @@ public class CharInfo : MonoBehaviour
     //public GameObject Point3;
     public GameObject Str1;
     public GameObject Wiz1;
+    public GameObject Hp1;
+    public GameObject Mp1;
+    public GameObject Hp2;
+    public GameObject Mp2;
+    //public GameObject Hp3;
+    //public GameObject Mp3;
     int StrCount;
     int WizCount;
     int StrCount2;
     int WizCount2;
     //int StrCount3;
     //int WizCount3;
-     public GameObject Str2;
+    int HpCount1;
+    int MpCount1;
+    int HpCount2;
+    int MpCount2;
+    //int HpCount3;
+    //int MpCount3;
+    public GameObject Str2;
      public GameObject Wiz2;
      /*public GameObject Str3;
      public GameObject Wiz3;*///  캐릭터3번생기면 여기쓰면됨
@@ -66,16 +78,66 @@ public class CharInfo : MonoBehaviour
             PointCount += -1;
         }
     }
+    public void HpUP1()
+    {
 
-        void Start()
+        if (PointCount >= 1)
+        {
+            HpCount1+=10;
+            this.Hp1.GetComponent<Text>().text = "" + (150 + HpCount1);
+            PointCount += -1;
+        }
+
+    }
+    public void MpUP1()
+    {
+
+        if (PointCount >= 1)
+        {
+            MpCount1+=10;
+            this.Mp1.GetComponent<Text>().text = "" + (200 + MpCount1);
+            PointCount += -1;
+        }
+    }
+    public void HpUP2()
+    {
+
+        if (PointCount >= 1)
+        {
+            HpCount2 += 10;
+            this.Hp2.GetComponent<Text>().text = "" + (200 + HpCount2);
+            PointCount += -1;
+        }
+
+    }
+    public void MpUP2()
+    {
+
+        if (PointCount >= 1)
+        {
+            MpCount2 += 10;
+            this.Mp2.GetComponent<Text>().text = "" + (150 + MpCount2);
+            PointCount += -1;
+        }
+    }
+
+    void Start()
     {
         
-        PointCount = 90;
+        PointCount = 360;
         this.Str1.GetComponent<Text>().text = " "+2+0;
         this.Wiz1.GetComponent<Text>().text = " "+5+0;
         this.Str2.GetComponent<Text>().text = " "+3+0;
         this.Wiz2.GetComponent<Text>().text = " "+3+0;
+        //this.Str3.GetComponent<Text>().text = " " + 3 + 0;
+        //this.Wiz3.GetComponent<Text>().text = " " + 3 + 0;
         //이부분에 위에와 같이 3번추가하면됨 위에 함수 21,22복사해서 31,32만들어서 인포3 버튼에 함수연결시켜주기
+        this.Hp1.GetComponent<Text>().text = " " + 150;
+        this.Mp1.GetComponent<Text>().text = " " + 200;
+        this.Hp2.GetComponent<Text>().text = " " + 200;
+        this.Mp2.GetComponent<Text>().text = " " + 150;
+        //this.Hp3.GetComponent<Text>().text = " " + 200;
+        //this.Mp3.GetComponent<Text>().text = " " + 200;
     }
 
     // Update is called once per frame
